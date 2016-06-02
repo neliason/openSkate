0b39f885f021bd213bdbe52430c92bfc904fe1a6
Nick Eliason <nceliason@gmail.com>
2016-06-02 03:53:15 +0000

    fixed syntax errors, added print statements

--- RPi Code/bluetooth_com.py
+++ RPi Code/bluetooth_com.py
@@ -1,6 +1,7 @@
 import re
 import RPi.GPIO as GPIO
 from bluetooth import *
+import bluetooth
 
 GPIO.setmode(GPIO.BCM)
 
@@ -13,32 +14,38 @@ def update_duty_cycle(dutyCycle):
 skate_sock=BluetoothSocket( RFCOMM )
 skate_sock.bind(("", PORT_ANY))
 skate_sock.listen(1)
+print "listening"
 
-port = server_sock.getsockname()[1]
+port = skate_sock.getsockname()[1]
+print "port", port
 
 uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-advertise_service( server_sock, "OpenSkate1",
+bluetooth.advertise_service( skate_sock, "OpenSkate1",
                    service_id = uuid,
                    service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ], 
-                    )
+                   profiles = [ SERIAL_PORT_PROFILE ],
+		 )
 
-while TRUE:
-	client_sock, client_info = server_sock.accept()
+while True:
+	client_sock, client_info = skate_sock.accept()
+	print "Accepted connection from", client_info
 
 	try:
 		incomingCMD = skate_sock.recv(1024)
+		print "incomingCMD", incomingCMD
+
 		#Parser for cmd type goes here
 		#Format should be: E0 D62 E(cmd type) D(value)
 		cmdType   = re.match(E+[0-9], incomingCMD)
-
+		print "cmdType", cmdType
 		if cmdType == 0:
 			dutyCycle = re.match(D+[0-9][0-9], incomingCMD)
 			update_duty_cycle(dutyCycle)
 
 		elif cmdType == 1:
 			#Other things that need values...
+			print "nothing"
 
 		
 	except IOError:
